89fa6f4e2a4c0857c5d98f59ca0e34e5d6eb7476
Sylvain Lesage <severo@rednegra.net>
2019-10-24 01:37:08 +0200

    build: also build ES module gathering all dependencies from node_modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,34 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import * as meta from './package.json'
 
-const config = (file, plugins) => ({
+const config = (file, plugins, format = 'umd') => ({
   input: 'index.js',
   output: {
     name: 'GeoEditor',
-    format: 'umd',
+    format,
     indent: false,
     extend: true,
     file
   },
-  plugins
+  plugins: [
+    resolve(),
+    commonjs(),
+    ...plugins
+  ]
 })
 
 export default [
+  config(`${meta.name}.esm.js`, [], 'esm'),
   config(`${meta.name}.js`, []),
-  config(`${meta.name}.min.js`, [terser({
-    output: {
-      preamble: `// ${meta.homepage} v${meta.version} Copyright ${(new Date()).getFullYear()} ${meta.author.name}`
-    }
-  })])
+  config(`${meta.name}.min.js`, [
+    terser({
+      output: {
+        preamble: `// ${meta.homepage} v${
+          meta.version
+        } Copyright ${new Date().getFullYear()} ${meta.author.name}`
+      }
+    })
+  ])
 ]
